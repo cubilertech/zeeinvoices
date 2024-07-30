69ba8b8c7d393d5633caa813f8e061553a10a49b
Muhammad Waqas <muhammadwaqas3447@gmail.com>
2024-07-30 16:17:28 +0500

    get token from google auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,45 @@
-// app/api/auth/[...nextauth]/route.ts
+// // app/api/auth/[...nextauth]/route.ts
 
-import NextAuth from 'next-auth';
+// import NextAuth from 'next-auth';
+// import GoogleProvider from 'next-auth/providers/google';
+
+
+// const handler = NextAuth({
+//   providers: [
+//         GoogleProvider({
+//           clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+//           clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
+//         }),
+//       ],
+// });
+
+// export {handler as GET ,handler as POST};
+
+
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
-        GoogleProvider({
-          clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-          clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
-        }),
-      ],
-});
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string
+      return session
+    },
+  },
+};
 
-export {handler as GET ,handler as POST};
+const handler = NextAuth(authOptions);
 
+export { handler as GET, handler as POST };
