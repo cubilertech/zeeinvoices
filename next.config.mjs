94ecfe3a4dec78c194387975a54f136b4563b0d3
sahil43hb <sahilmubeen30@gmail.com>
2024-08-07 14:52:25 +0100

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['localhost', 'lh3.googleusercontent.com'],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    domains: process.env.NEXT_PUBLIC_IMAGES_DOMAINS.split(','),
+    // domains: ['localhost','lh3.googleusercontent.com'],
+  },
+};
+
+export default nextConfig;
