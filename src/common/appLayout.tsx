c1d155f79e89207a079961a0fd6965bd6025f698
Eng-Abdul-Rehman <eng.mirza.rehman@gmail.com>
2024-09-10 18:19:34 +0500

    done

--- src/common/appLayout.tsx
+++ src/common/appLayout.tsx
@@ -1,25 +1,30 @@
 "use client";
-import { Footer } from "@/components/Footer";
+import { FooterSection } from "@/appPages/SecondLandingPage/FooterSection";
+// import { FooterSection } from "@/appPages/LandingPage/FooterSection";
+import { Footer as DesktopFooter } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { palette } from "@/theme/palette";
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 const AppLayout = ({ children }: { children: ReactNode }) => {
   const pathname = usePathname();
+  const isModile = useMediaQuery("(max-width: 500px)");
   console.log(pathname, "current route");
   return (
     <>
-      {/* // <Box sx={{ background: palette.color.lightWhite, height: "100%" }}> */}
+      {/* <Box sx={{ background: palette.color.lightWhite, height: "100vh" }}> */}
       <Header />
       {children}
       {pathname == "/" ||
       pathname == "/termsAndCondition" ||
       pathname == "/contact-us" ? (
         <></>
+      ) : isModile ? (
+        <FooterSection />
       ) : (
-        <Footer />
+        <DesktopFooter />
       )}
       {/* </Box> */}
     </>
