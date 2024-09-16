80d78d7bb8b31dc0b72c9c404cc2e9da525d10c8
AadilYusuf <aadilyusuf99@gmail.com>
2024-09-16 18:54:08 +0500

    search type text

--- src/components/AllInvoices/enhancedTableToolbar.tsx
+++ src/components/AllInvoices/enhancedTableToolbar.tsx
@@ -89,7 +89,7 @@ const EnhancedTableToolbar: FC<EnhancedTableToolbarProps> = (
         <TextField
           variant="standard"
           placeholder="Search"
-          type="number"
+          // type="number"
           value={search}
           onChange={(e) => handleChangeSearch(e)}
           sx={{
