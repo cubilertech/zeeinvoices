17cfdea2cc58dd58d71a3c5c50625518ec0b1983
AadilYusuf <aadilyusuf99@gmail.com>
2024-10-14 11:52:01 +0500

    login redirection

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -47,7 +47,7 @@ export const handleLogin = (pathname = "/") => {
       `${
         process.env.NEXT_PUBLIC_GOOGLE_CALLBACK_URL
       }${"/create-new-invoice"}` ||
-      "https://main.d33zziho5sqnl9.amplifyapp.com",
+      "https://zeeinvoices.com/create-new-invoice",
   });
 };
 //Image Base 64 Convertion
