7d15ea7f4f4bbef15c4767cba0f901306d88b655
Muhammad Waqas <muhammadwaqas3447@gmail.com>
2024-12-26 11:13:38 +0500

    testing state reset code

--- src/common/appLayout.tsx
+++ src/common/appLayout.tsx
@@ -4,10 +4,25 @@ import { Header } from "@/components/Header";
 import { Box, useMediaQuery } from "@mui/material";
 import { usePathname } from "next/navigation";
 import { ReactNode, useEffect } from "react";
+import { purgeStoredState } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const AppLayout = ({ children }: { children: ReactNode }) => {
   const pathname = usePathname();
   const isModile = useMediaQuery("(max-width: 500px)");
+  const currentStateVersion = '1';
+  const clearReduxState = async () => {
+   const val = localStorage.getItem('__persistedVersion-zeeinvoices');
+  if(!val || val && val !== currentStateVersion){
+   console.log("Clearing old state due to version mismatch.");
+   await purgeStoredState({ key: "root", storage });
+   localStorage.setItem('__persistedVersion-zeeinvoices',currentStateVersion);
+   window.location.reload(); // Reload to apply the cleared state
+  }
+  }
+ useEffect(()=>{
+   clearReduxState();
+ },[]);
   // const dispatch = useDispatch();
   // useEffect(() => {
   //   const handleBeforeUnload = () => {  // redux initial state if browser close but it also clear the redux if save invoice without login
